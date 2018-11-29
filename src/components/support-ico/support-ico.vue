68cbf1b816788674611149790c6d6f66a082d98f
psrheartache <chenmr.cyf@qq.com>
2018-11-29 09:43:51 +0800

    pro init

---
+++ src/components/support-ico/support-ico.vue
@@ -0,0 +1,32 @@
+<template>
+	<span class="support-ico" :span="iconCls"></span>
+</template>
+
+<script>
+	export default{
+		name:  'support-ico',
+		props: {
+			size:{
+				type: Number,
+			},
+			type:{
+				type: Number
+			}
+		},
+		computed: {
+			iconCls(){
+				const classMap = ['decrease','discount','special','invoice','guarantee']
+				return `icon-${this.size} ${classMap[this.type]}`
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+		@import "~common/stylus/mixin"
+		@import "~common/stylus/variable"
+		
+		.support-ico
+		  display: inline-block
+		  background-repeat: no-repeat		
+</style>
